bf1994f51a4f1886e3d91133b7999d0b8f8f8c09
Frogpants <headmaster3.pro@gmail.com>
2023-09-28 18:41:39 +0000

    hello

--- side_projects/game.py
+++ side_projects/game.py
@@ -140,7 +140,7 @@ def xmove(s):
     global xcam
     xcam += s
 
-def tri(x,y,z,a,b,c,d,e,f):  
+def draw_line(x,y,z,a,b,c,d,e,f):  
 
     #First Point
     p(x,y,z)
@@ -151,92 +151,16 @@ def tri(x,y,z,a,b,c,d,e,f):
     p(a,b,c)
     p(px-xcam,py-ycam,pz-zcam)
     point2(px,py,pz)
-    
-    #Third Point
-    p(d,e,f)
-    p(px-xcam,py-ycam,pz-zcam)
-    point3(px,py,pz)
 
-    tr.penup()
-
-    if (z1 > 0) and (z2 > 0) and (z3 > 0):
-        #Project onto Screen
-
-        p(300 * (x1/z1), 300 * (y1/z1),z1)
-        onscreen1(px,py)
-
-        p(300 * (x2/z2), 300 * (y2/z2),z2)
-        onscreen2(px,py)
-
-        p(300 * (x3/z3),300 * (y3/z3),z3)
-        onscreen3(px,py)
-
-        fill(x1,y1,x2,y2,x3,y3,1)
-
-def plane(x,y,z,xs,ys,zs):
-    tri(x-xs,y-ys,z+zs,x-xs,y+ys,z+zs,x+xs,y+ys,z+zs)
-    tri(x+xs,y-ys,z+zs,x+xs,y+ys,z+zs,x-xs,y-ys,z+zs)
+def z_clip(x1,y1,z1,x2,y2,z2):
+    point1()
 
 def cube(x,y,z,xs,ys,zs):
-    #Front and back
-    plane(x,y,z+zs,xs,ys,1)
-    plane(x,y,z-zs,xs,ys,1)
-
-    #Sides
-    plane(x+xs,y,z,1,ys,zs)
-    plane(x-xs,y,z,1,ys,zs)
-
-    #Top and Bottom
-    plane(x,y+ys,z,xs,1,zs)
-    plane(x,y-ys,z,xs,1,zs)
+    return
 
 def render():
     cube(0,0,0,50,50,50)
 
-#Triangle Fill
-
-def fill(ax,ay,bx,by,cx,cy,r):
-    global wireframe
-    wireframe = 0
-    if wireframe == 0:
-        lena = m.sqrt(((bx-cx)*(bx-cx))+((by-cy)*(by-cy)))
-        lenb = m.sqrt(((ax-cx)*(ax-cx))+((ay-cy)*(ay-cy)))
-        lenc = m.sqrt(((ax-bx)*(ax-bx))+((ay-by)*(ay-by)))
-        peri = 1/((lena+lenb)+lenc)
-        incx = (((lena*ax)+(lenb*bx))+(lenc*cx))*peri
-        incy = (((lena*ay)+(lenb*by))+(lenc*cy))*peri
-        ind = m.sqrt(((lenc+lenb-lena)*(lenc+lena-lenb)*(lena+lenb-lenc))*peri)
-        aox = incx-ax
-        aoy = incy-ay
-        box = incx-bx
-        boy = incy-by
-        cox = incx-cx
-        coy = incy-cy
-        if (lena < lenb) and (lena < lenc):
-            td = m.sqrt((aox*aox)+(aoy*aoy))
-        else:
-            if (lenb > lena) or (lenb > lenc):
-                td = m.sqrt((cox*cox)+(coy*coy))
-            else:
-                td = m.sqrt((box*box)+(boy*boy))
-        rate = ((td*2)-ind)/(td*4)
-        td = 1+0
-        tr.goto(round(incx,1),round(incy,1))
-        tr.pensize(ind)
-        tr.pendown()
-        for i in range(m.ceil((m.log(r/ind))/(m.log(rate)))):
-            td = td*rate
-            tr.pensize(ind*td)
-            tr.goto((aox*td)+ax,(aoy*td)+ay)
-            tr.goto((box*td)+bx,(boy*td)+by)
-            tr.goto((cox*td)+cx,(coy*td)+cy)
-            tr.goto((aox*td)+ax,(aoy*td)+ay)
-        tr.pensize(r)
-        tr.goto(ax,ay)
-        tr.goto(bx,by)
-        tr.goto(cx,cy)
-        tr.goto(ax,ay)
-        tr.penup()
 
 #Order
 
